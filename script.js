2a0bf426c046a9cf7fd23912e70839d6ecf9cb4b
Bhakti <bhaktinirbhaybhatt@gmail.com>
2023-07-08 12:39:29 +0530

    function created for each event

--- script.js
+++ script.js
@@ -7,101 +7,49 @@ document.body.addEventListener(
     //   console.log(`${event.key}`);
     if (keyid === 65) {
       console.log("a was pressed");
-      var A = document.getElementById("A");
-      var boom = new Audio("./assets/DrumsetAudioFiles/boom.wav");
-      boom.play();
-      A.style.scale = "1.1";
-      A.style.boxShadow = "0px 0px 11px 1px0 rgba(148,144,214,1)";
-      beatName.innerText = '"boom"';
-      showBeat();
+      Akey();
+     
     } 
     
     else if (keyid === 83) {
       console.log("s was pressed");
-      var S = document.getElementById("S");
-      var clap = new Audio("./assets/DrumsetAudioFiles/clap.wav");
-      clap.play();
-      S.style.scale = "1.1";
-      S.style.boxShadow = "0px 0px 11px 1px rgba(148,144,214,1)";
-      beatName.innerText = '"clap"';
-      showBeat();
+      Skey();
     } 
     
     else if (keyid === 68) {
       console.log("d was pressed");
-      var D = document.getElementById("D");
-      var hihat = new Audio("./assets/DrumsetAudioFiles/hihat.wav");
-      hihat.play();
-      D.style.scale = "1.1";
-      D.style.boxShadow = "0px 0px 11px 1px rgba(148,144,214,1)";
-      beatName.innerText = '"hihat"';
-      showBeat();
+      Dkey()
     }
     
     else if (keyid === 70) {
       console.log("f was pressed");
-      var F = document.getElementById("F");
-      var kick = new Audio("./assets/DrumsetAudioFiles/kick.wav");
-      kick.play();
-      F.style.scale = "1.1";
-      F.style.boxShadow = "0px 0px 11px 1px rgba(148,144,214,1)";
-      beatName.innerText = '"kick"';
-      showBeat();
+      Fkey();
     } 
     
     else if (keyid === 71) {
       console.log("g was pressed");
-      var G = document.getElementById("G");
-      var openhat = new Audio('./assets/DrumsetAudioFiles/openhat.wav')
-      openhat.play();
-      G.style.scale = "1.1";
-      G.style.boxShadow = "0px 0px 11px 1px rgba(148,144,214,1)";
-      beatName.innerText = '"openhat"';
-      showBeat();
+      Gkey();
     }
     
     else if (keyid === 72) {
       console.log("h was pressed");
-      var H = document.getElementById("H");
-      var ride = new Audio("./assets/DrumsetAudioFiles/ride.wav");
-      ride.play();
-      H.style.scale = "1.1";
-      H.style.boxShadow = "0px 0px 11px 1px rgba(148,144,214,1)";
-      beatName.innerText = '"ride"';
-      showBeat();
+      Hkey();
     }
     
     else if (keyid === 74){
       console.log("j was pressed");
-      var J = document.getElementById("J");
-      var snare = new Audio("./assets/DrumsetAudioFiles/snare.wav");
-      snare.play();
-      J.style.scale = "1.1";
-      J.style.boxShadow = "0px 0px 11px 1px rgba(148,144,214,1)";
-      beatName.innerText = '"snare"';
-      showBeat();
+      Jkey();
+     
     }
     
     else if (keyid === 75) {
       console.log("k was pressed");
-      var K = document.getElementById("K");
-      var tink = new Audio("./assets/DrumsetAudioFiles/tink.wav");
-      tink.play();
-      K.style.scale = "1.1";
-      K.style.boxShadow = "0px 0px 11px 1px rgba(148,144,214,1)";
-      beatName.innerText = '"tink"';
-      showBeat();
+      Kkey();
     }
     
     else if (keyid === 76) {
       console.log("l was pressed");
-      var L = document.getElementById("L");
-      var tom = new Audio("./assets/DrumsetAudioFiles/tom.wav");
-      tom.play();
-      L.style.scale = "1.1";
-      L.style.boxShadow = "0px 0px 11px 1px rgba(148,144,214,1)";
-      beatName.innerText = '"tom"';
-      showBeat()
+      Lkey();
     } else {
       console.log("different key pressed");
       showBeat();
@@ -201,6 +149,91 @@ document.body.addEventListener(
     true
   );
   
+  function Akey() {
+    var A = document.getElementById("A");
+    A.style.scale = "1.1";
+    A.style.boxShadow = "0px 0px 11px 1px0 rgba(148,144,214,1)";
+    var boom = new Audio("./assets/DrumsetAudioFiles/boom.wav");
+    boom.play();
+    beatName.innerText = '"boom"';
+    showBeat();   
+  }
+  function Skey() {
+    var S = document.getElementById("S");
+    S.style.scale = "1.1";
+    S.style.boxShadow = "0px 0px 11px 1px rgba(148,144,214,1)";
+    var clap = new Audio("./assets/DrumsetAudioFiles/clap.wav");
+    clap.play();
+    beatName.innerText = '"clap"';
+    showBeat();
+    
+  }
+  function Dkey() {
+    var D = document.getElementById("D");
+    D.style.scale = "1.1";
+    D.style.boxShadow = "0px 0px 11px 1px rgba(148,144,214,1)";
+    var hihat = new Audio("./assets/DrumsetAudioFiles/hihat.wav");
+    hihat.play();
+    beatName.innerText = '"hihat"';
+    showBeat();
+    
+  }
+  function Fkey(){
+    var F = document.getElementById("F");
+    F.style.scale = "1.1";
+    F.style.boxShadow = "0px 0px 11px 1px rgba(148,144,214,1)";
+    var kick = new Audio("./assets/DrumsetAudioFiles/kick.wav");
+    kick.play();
+    beatName.innerText = '"kick"';
+    showBeat();
+  }
+  
+  function Gkey() {
+    var G = document.getElementById("G");
+    G.style.scale = "1.1";
+    G.style.boxShadow = "0px 0px 11px 1px rgba(148,144,214,1)";
+    var openhat = new Audio('./assets/DrumsetAudioFiles/openhat.wav')
+    openhat.play();
+    beatName.innerText = '"openhat"';
+    showBeat();
+    
+  }
+  function Hkey() {
+    var H = document.getElementById("H");
+    H.style.scale = "1.1";
+    H.style.boxShadow = "0px 0px 11px 1px rgba(148,144,214,1)";
+    var ride = new Audio("./assets/DrumsetAudioFiles/ride.wav");
+    ride.play();
+    beatName.innerText = '"ride"';
+    showBeat();
+  }
+  function Jkey() {
+    var J = document.getElementById("J");
+    J.style.scale = "1.1";
+    J.style.boxShadow = "0px 0px 11px 1px rgba(148,144,214,1)";
+    var snare = new Audio("./assets/DrumsetAudioFiles/snare.wav");
+    snare.play();
+    beatName.innerText = '"snare"';
+    showBeat();
+  }
+  function Kkey() {
+    var K = document.getElementById("K");
+    K.style.scale = "1.1";
+    K.style.boxShadow = "0px 0px 11px 1px rgba(148,144,214,1)";
+    var tink = new Audio("./assets/DrumsetAudioFiles/tink.wav");
+    tink.play();
+    beatName.innerText = '"tink"';
+    showBeat();
+  }
+  function Lkey() {
+    var L = document.getElementById("L");
+    L.style.scale = "1.1";
+    L.style.boxShadow = "0px 0px 11px 1px rgba(148,144,214,1)";
+    var tom = new Audio("./assets/DrumsetAudioFiles/tom.wav");
+    tom.play();
+    beatName.innerText = '"tom"';
+    showBeat()
+  }
   function showBeat() {
     setTimeout(function(){
     beatName.innerText = '"beat"';
